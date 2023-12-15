99d4301baa0aa201a3383441ef2c20a7b4486f2a
laurencetuchin <laurencetuchin@gmail.com>
2023-12-15 17:06:53 +1100

    Add Valid & Game function

--- script.js
+++ script.js
@@ -34,51 +34,90 @@ function reset() {
 	return playerChoice;
 }
 
+// valid choice check
+function valid(validSelection) {
+	validSelection = getPlayerChoice(validSelection);
+	if (choice.includes(validSelection)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
 // score helper
 function scoreHelper(number, playerChoice, computerChoice) {
+	// if (number === 1 ? playerScore++ : computerScore++)
+	if (computerScore === 3) {
+		return "Computer wins";
+	}
+	if (playerScore === 3) {
+		return "Player wins";
+	}
+
 	if (number === 1) {
+		playerScore++;
 		return `Player 1 wins, ${playerChoice} beats ${computerChoice}`;
 	} else {
+		computerScore++;
 		return `Computer wins, ${computerChoice} beats ${playerChoice}`;
 	}
 }
 // create a function that plays a round of RPS
 function playRPS(playerChoice, computerChoice) {
-	// if choice is the same = draw and replay
-	if (playerChoice === computerChoice) {
-		// playRPS(playerChoice, computerChoice);
-		console.log("Draw");
-		// reset();
-		playerChoice = reset();
-		computerChoice = getComputerChoice();
-		playRPS(playerChoice, computerChoice);
-	}
-	// check for player 1 win scenario
-	if (playerChoice === "Rock" && computerChoice === "Scissors") {
-		scoreHelper(1, playerChoice, computerChoice);
-	}
+	// check selection is valid
+	if (valid(playerChoice)) {
+		// if choice is the same = draw and replay
+		if (playerChoice === computerChoice) {
+			// playRPS(playerChoice, computerChoice);
+			console.log("Draw");
+			// reset();
+			playerChoice = reset();
+			computerChoice = getComputerChoice();
+			playRPS(playerChoice, computerChoice);
+		}
+		// check for player 1 win scenario
+		if (playerChoice === "Rock" && computerChoice === "Scissors") {
+			scoreHelper(1, playerChoice, computerChoice);
+		}
 
-	if (playerChoice === "Paper" && computerChoice === "Rock") {
-		return scoreHelper(1, playerChoice, computerChoice);
-	}
-	if (playerChoice === "Rock" && computerChoice === "Scissors") {
-		return scoreHelper(1, playerChoice, computerChoice);
+		if (playerChoice === "Paper" && computerChoice === "Rock") {
+			return scoreHelper(1, playerChoice, computerChoice);
+		}
+		if (playerChoice === "Rock" && computerChoice === "Scissors") {
+			return scoreHelper(1, playerChoice, computerChoice);
+		} else {
+			// player 1 cannot draw or win so must lose
+			return scoreHelper(0, playerChoice, computerChoice);
+		}
 	} else {
-		// player 1 cannot draw or win so must lose
-		return scoreHelper(0, playerChoice, computerChoice);
+		throw Error("Invalid choice");
 	}
 }
 
-console.log(playRPS(playerChoice, computerChoice));
+// console.log(playRPS(playerChoice, computerChoice));
 
 function game() {
 	// playRPS(playerChoice, computerChoice);
+
 	for (let i = 0; i < 5; i++) {
 		// reset();
-		playRPS(playerChoice, computerChoice);
+
+		console.log(playRPS(playerChoice, computerChoice));
+		playerChoice = reset();
+		computerChoice = getComputerChoice();
+
+		if (playerScore >= 3) {
+			console.log("Player 1 Wins");
+		}
+		if (computerScore >= 3) {
+			console.log("Computer wins");
+		}
 	}
 }
-// game();
+game();
 
 // function game(playRPS) {}
 
